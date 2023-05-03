2eccdc0b22bed2498742055f02801dc83f97f385
IrishFix <benknight988@gmail.com>
2023-05-04 00:38:17 +0100

    -Add Newtonsoft.Json dll to enable me to json encode without external packages

--- Runtime/Networking/MLP.cs
+++ Runtime/Networking/MLP.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
 using UniversalNumerics.Networking.Layers;
+using Newtonsoft.Json;
 
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable once CheckNamespace
